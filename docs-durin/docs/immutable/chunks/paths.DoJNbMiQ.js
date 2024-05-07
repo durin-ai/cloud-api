d594a07e456f88e7d8df32be9046bfb36a47683a
Frederik (P0x00) <fdelacou@gmail.com>
2024-05-07 17:11:57 -0500

    update

--- docs-durin/docs/immutable/chunks/paths.DoJNbMiQ.js
+++
@@ -1,1 +0,0 @@
-var s;const e=((s=globalThis.__sveltekit_111cqnx)==null?void 0:s.base)??"/cloud-api";var a;const t=((a=globalThis.__sveltekit_111cqnx)==null?void 0:a.assets)??e;export{t as a,e as b};
